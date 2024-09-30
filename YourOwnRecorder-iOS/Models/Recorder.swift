cf3199f59224be58a3e0d0843a311d5952d5b889
sgmkhr <mksghr10@gmail.com>
2024-10-01 07:24:03 +0900

    Recorder一覧画面作成

---
+++ YourOwnRecorder-iOS/Models/Recorder.swift
@@ -0,0 +1,27 @@
+//
+//  Recorder.swift
+//  YourOwnRecorder-iOS
+//
+//  Created by 菅原実希 on 2024/10/01.
+//
+
+import Foundation
+
+struct Recorder {
+    var title: String
+    var description: String? = nil
+    var contents: [RecorderItem]
+    var createdDate: Date
+    var updatedDate: Date
+    var isShowed: Bool = true
+}
+
+#if DEBUG
+extension Recorder {
+    static var sampleData = [
+        Recorder(title: "Runnning", description: "To record runnning time and distance daily.", contents: [.textField, .textArea], createdDate: Date().addingTimeInterval(-800.0), updatedDate: Date().addingTimeInterval(-600.0), isShowed: true),
+        Recorder(title: "Coffee", description: nil, contents: [.slider, .slider, .slider, .textField, .textArea], createdDate: Date().addingTimeInterval(-1000.0), updatedDate: Date().addingTimeInterval(-800.0), isShowed: true),
+        Recorder(title: "Visited Prefectures", description: "Just preparing for a next trip", contents: [], createdDate: Date().addingTimeInterval(-100.0), updatedDate: Date().addingTimeInterval(-100.0), isShowed: false)
+    ]
+}
+#endif
---
+++ YourOwnRecorder-iOS/Models/RecorderItem.swift
@@ -0,0 +1,20 @@
+//
+//  RecorderItem.swift
+//  YourOwnRecorder-iOS
+//
+//  Created by 菅原実希 on 2024/10/01.
+//
+
+import Foundation
+
+enum RecorderItem {
+    case textField
+    case textArea
+    case slider
+    case dateField
+    
+    //var title: String? = nil
+    //var content = switch self {
+    //case .textField:
+        
+}
