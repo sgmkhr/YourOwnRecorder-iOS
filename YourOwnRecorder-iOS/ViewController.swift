a75f6ba1f673bfa9efde62626139298a4e7d69e1
sgmkhr <mksghr10@gmail.com>
2024-10-01 06:02:40 +0900

    Initial commit

---
+++ YourOwnRecorder-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourOwnRecorder-iOS
+//
+//  Created by 菅原実希 on 2024/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
