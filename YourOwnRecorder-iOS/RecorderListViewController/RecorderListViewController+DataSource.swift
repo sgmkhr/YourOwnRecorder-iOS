1dd5c9d4d9b2f9aa9c8f34f0b6f5cfef4a663be2
sgmkhr <mksghr10@gmail.com>
2024-10-01 22:36:43 +0900

    recorder一覧　セルの表示設定

---
+++ YourOwnRecorder-iOS/RecorderListViewController/RecorderListViewController+DataSource.swift
@@ -0,0 +1,26 @@
+//
+//  RecorderListViewController+DataSource.swift
+//  YourOwnRecorder-iOS
+//
+//  Created by 菅原実希 on 2024/10/01.
+//
+
+import UIKit
+
+extension RecorderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let recorder = Recorder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = recorder.title
+        contentConfiguration.secondaryText = recorder.updatedDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = UIColor(red: 254/255, green: 247/255, blue: 1, alpha: 1)
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+}
